730630ec6f0b53ec822b8e6a0284f59d97240521
Melany <123525805+melanyscode@users.noreply.github.com>
2024-03-10 19:51:33 -0600

    Menu en proceso
    Realizacion del menu, menu inventario de productos y gestion de trabajos realizados hecho. Falta hacer el resto de modulos

--- Lubricentro/src/main/java/ModuloCompraVenta/Cola.java
+++ Lubricentro/src/main/java/ModuloCompraVenta/Cola.java
@@ -2,13 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package BD;
+package ModuloCompraVenta;
 
 /**
  *
  * @author Melanie Gutierrez
  */
-public class Conection {
-    //crear conexion con la base de datos 
-    //crear metodos de insertar, eliminar y consulta con la base de datos para utilizarlo en el proyecto
+public class Cola {
+    
 }
