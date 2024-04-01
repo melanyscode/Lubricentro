e180fc24657580f0b37cbee300c696ab0177f731
Melany <123525805+melanyscode@users.noreply.github.com>
2024-04-01 16:57:39 -0600

    Cola gestion de compra y venta
    cola y nodo par agestion de compra y venta

--- Lubricentro/src/main/java/ModuloCompraVenta/Cola.java
+++ Lubricentro/src/main/java/ModuloCompraVenta/Cola.java
@@ -4,10 +4,71 @@
  */
 package ModuloCompraVenta;
 
+import Objetos.Producto;
+
 /**
  *
  * @author Melanie Gutierrez
  */
 public class Cola {
+
+    private NodoCola inicio;
+    private NodoCola fin;
+
+    public Cola() {
+    }
+
+    //metodos de la cola
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public void agregar(Producto p) {
+        NodoCola nuevo = new NodoCola(p);
+        if (isEmpty()) {
+            inicio = nuevo;
+        } else {
+            fin.setSiguiente(nuevo);
+        }
+        fin = nuevo;
+    }
+
+    public Producto desencolar() {
+        Producto aux = inicio.getProducto();
+        inicio.setSiguiente(inicio);
+        return aux;
+    }
+
+    public Producto inicioCola() {
+        return inicio.getProducto();
+    }
+
+    public String imprimirCola() {
+        String mensaje = "";
+        if (isEmpty()) {
+            mensaje += "No hay productos en la cola de compra";
+        } else {
+            mensaje += "Lista de Productos:\n";
+            NodoCola aux = inicio;
+            while (aux != null) {
+                mensaje += aux.getProducto().getNombre() + " - ₡" + aux.getProducto().getPrecio() + "\n";
+                aux = aux.getSiguiente();
+            }
+        }
+        return mensaje;
+    }
     
+    public double precioTotal(){
+        double res = 0;
+        if(isEmpty()){
+            res = 0;
+        }else{
+            NodoCola aux = inicio;
+            while(aux != null){
+                res += aux.getProducto().getPrecio();
+                aux = aux.getSiguiente();
+            }
+        }
+        return res;
+    }
 }
